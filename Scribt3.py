b31285ab0fa388f361cf091dea93981508338011
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-04-01 09:52:49 +0700

    Update Scribt3.py

--- Scribt3.py
+++ Scribt3.py
@@ -144,7 +144,7 @@ def generate_report(df, tipe, zero_option):
         df['COMMISSION'] = df['KOMISI_SP']
         df['CLAIM'] = df['KLAIM_SP']
 
-    df['AMOUNT'] = df['PREMIUM'] - df['COMMISSION']
+    df['AMOUNT'] = df['PREMIUM'] - df['COMMISSION'] - df['CLAIM']
 
     grouped = (
         df.groupby(['CURRENCY','COB','UY'])
